fb746ae09b8929b6b3b0fb61bfbc020099a0e075
Maligno.Snow <msnow.catch@gmail.com>
2021-08-09 21:36:16 -0500

    plantilla cocacola

---
+++ js/funciones.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(runner);
+
+function runner() {
+    jQuery('.hamb').click(function(e) {
+        e.preventDefault();
+        jQuery('header .container nav').toggleClass('open');
+        jQuery('.hamb i').toggleClass('fa-times');
+    })
+
+    jQuery('header .container nav a').click(function() {
+
+        jQuery('header .container nav').removeClass('open');
+        jQuery('.hamb i').removeClass('fa-times');
+
+        var dev = jQuery(this).attr('href');
+        
+        jQuery('html, body').animate({
+            'scrollTop': jQuery(dev).offset().top
+        })
+    })
+}
+
